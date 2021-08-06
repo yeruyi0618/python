52fe88ba4074594cb308f334acb9cfcb231fecbe
yeruyi0618 <544619578@qq.com>
2021-08-06 14:27:30 +0800

    add if循环文件

---
+++ if循环.py
@@ -0,0 +1,10 @@
+# coding=utf-8
+
+
+user = input("Please input your username: ")
+psd = input("Please input your passworld: ")
+
+if user == 'yeruyi0618' and psd == 'yeruyi@20181225':
+    print ('yes')
+else:
+    print ('no')
